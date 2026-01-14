cf6338e85c3ec7f5fe879f22b5515ba661a4d2b8
Soham Shukla <sohammshukla@gmail.com>
2026-01-13 22:34:55 -0600

    current version w/ center-bottom color display and single image capability

--- color_detection.py
+++ color_detection.py
@@ -9,26 +9,31 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help = "image path")
 arg = vars(ap.parse_args())
 path = arg["image"]
-image = cv2.imread(path)
+img = cv2.imread(path)
+
+
 
 # image categorical data from csv file
 
 index = ["color", "color_name", "hex", "R", "G", "B"]
 csv = pd.read_csv('colors.csv', names=index, header=None)
+clicked = False
+xpos = ypos = 0
+b = g = r = 0
 
 
 # drawFunction calculates RGB values w/ a doubleclick
 
 def drawFunction(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        global r,g,b, xpos, ypos, clicked
+    if event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r, xpos, ypos, clicked
         clicked = True
-        xpos = x
-        ypos = y
-        r,g,b = image[x,y]
-        r = int(r)
-        g = int(g)
+        xpos = img.shape[1] // 2
+        ypos = img.shape[0] - 1
+        b,g,r = img[y,x]
         b = int(b)
+        g = int(g)
+        r = int(r)
 
 # distance calculation to find most accurate color_name
 
@@ -39,27 +44,31 @@ def getColorName(R, G, B):
 
         if d <= minimum:
             minimum = d
-            colorName = csv.loc[i, "colorName"]
-        return colorName
-    
-while True:
-    cv2.imshow("currentImage", image)
-    if clicked:
+            colorName = csv.loc[i, "color_name"]
+
+    return colorName
+
+# window application
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', drawFunction)
+
+# runtime program
+
+while (True):
+    display = img.copy()
+    if (clicked) :
         # generate rectange to show highlighted section on image
-        cv2.rectangle(image, (50,50), (100,100), (r,g,b), 10)
+        cv2.rectangle(display, (xpos - 300,ypos), (xpos + 300,ypos - 40), (b,g,r), -1)
 
         # display text for colorName
         text = getColorName(r,g,b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
-        cv2.putText(image, text, (75,75), 3, 1, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(display, text, (xpos - 300,ypos), 3, 1, (0,0,0), 2, cv2.LINE_AA)
 
         clicked = False
 
+    cv2.imshow("image", display)
 
-        if cv2.waitKey(50) & 0xFF == 27:
-            break
+    if cv2.waitKey(500) & 0xFF == 27:
+        break
 
 cv2.destroyAllWindows()
-
-# window application
-cv2.namedWindow('currentImage')
-cv2.setMouseCallback('currentImage', drawFunction())
